f5f468924938e88bb1df5fc13054ab21a3d54a53
ValentinaNovarino <valenovarino@gmail.com>
2020-11-11 11:06:43 +0100

    Stampato lista

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            title: 'lista della spesa',
+            shoppingList: [
+                'Pasta',
+                'Verdura',
+                'Frutta',
+                'Detersivi',
+                'Carne'
+            ]
+        },
+    }
+);
